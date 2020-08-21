c863a955892a9dc934f2f997221631aae52d9173
zhangyanhe <zhangyanhe@maka.im>
2020-08-21 08:48:47 +0800

    fix bug

--- dynamic/coins_v2_test.go
+++ dynamic/coins_v2_test.go
@@ -29,3 +29,31 @@ func Test_changeV1(t *testing.T) {
 		})
 	}
 }
+
+func Test_changeV2(t *testing.T) {
+	type args struct {
+		amount int
+		coins  []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"want 4",
+			args{
+				amount: 5,
+				coins:  []int{1, 2, 5},
+			},
+			4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := changeV2(tt.args.amount, tt.args.coins); got != tt.want {
+				t.Errorf("changeV2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
