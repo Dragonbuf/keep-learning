a6b783586c86730a1115cdd62cc52274c4dc7405
zhangyanhe <zhangyanhe@maka.im>
2020-08-24 09:03:51 +0800

    最长公共子序列

---
+++ dynamic/subSequnce_test.go
@@ -0,0 +1,48 @@
+package dynamic
+
+import "testing"
+
+func Test_longestCommonSubsequence(t *testing.T) {
+	type args struct {
+		str1 string
+		str2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"can see 3",
+			args{
+				str1: "abcde",
+				str2: "ace",
+			},
+			3,
+		},
+		{
+			"can see 3",
+			args{
+				str1: "abc",
+				str2: "abc",
+			},
+			3,
+		},
+		{
+			"can see 0",
+			args{
+				str1: "abc",
+				str2: "def",
+			},
+			0,
+		},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestCommonSubsequence(tt.args.str1, tt.args.str2); got != tt.want {
+				t.Errorf("longestCommonSubsequence() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
