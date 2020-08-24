adef17de855f5840da1af207f1fb2fe4e6e61d8c
zhangyanhe <zhangyanhe@maka.im>
2020-08-24 08:34:35 +0800

    钱包问题 v2

--- dynamic/coins_v2.go
+++ dynamic/coins_v2.go
@@ -36,3 +36,31 @@ func changeV2(amount int, coins []int) int {
 
 	return dp[amount]
 }
+
+// 戳气球问题
+//https://mp.weixin.qq.com/s/I0yo0XZamm-jMpG-_B3G8g
+func maxCoins(nums []int) int {
+
+	n := len(nums)
+	points := make([]int, n+2)
+	points[0] = 1
+	points[n+1] = 1
+	for k := range nums {
+		points[k+1] = nums[k]
+	}
+
+	dp := make([][]int, n+2)
+	for k := range dp {
+		dp[k] = make([]int, n+2)
+	}
+
+	for i := n; i >= 0; i-- {
+		for j := i + 1; j <= n+1; j++ {
+			for k := i + 1; k < j; k++ {
+				dp[i][j] = max(dp[i][j], dp[i][k]+dp[k][j]+points[i]*points[j]*points[k])
+			}
+		}
+	}
+
+	return dp[0][n+1]
+}
--- dynamic/coins_v2_test.go
+++ dynamic/coins_v2_test.go
@@ -57,3 +57,27 @@ func Test_changeV2(t *testing.T) {
 		})
 	}
 }
+
+func Test_maxCoins(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"can see 167",
+			args{[]int{3, 1, 5, 8}},
+			167,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxCoins(tt.args.nums); got != tt.want {
+				t.Errorf("maxCoins() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
