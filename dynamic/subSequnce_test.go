840b350bcbfcd5845dfb68e06ee6ac22e39724d2
zhangyanhe <zhangyanhe@maka.im>
2020-08-25 08:51:49 +0800

    最长回文子序列

--- dynamic/subSequnce_test.go
+++ dynamic/subSequnce_test.go
@@ -46,3 +46,27 @@ func Test_longestCommonSubsequence(t *testing.T) {
 		})
 	}
 }
+
+func Test_longestPalindromeSubseq(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"can see 4",
+			args{s: "bbbab"},
+			4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindromeSubseq(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindromeSubseq() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
