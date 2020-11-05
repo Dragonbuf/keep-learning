0a158e7a7095f6934c54149be53946884749c058
zhangyanhe <zhangyanhe@maka.im>
2020-11-05 18:59:29 +0800

    update new dfs

--- v2/binaryTree/binaryTree.go
+++ v2/binaryTree/binaryTree.go
@@ -238,20 +238,28 @@ func pathSum(root *TreeNode, sum int) [][]int {
     dfs(root,sum,[]int{},&ret)
     return ret
 }
+
 func dfs(root *TreeNode,sum int,arr []int,ret *[][]int){
     if root == nil{
         return
     }
     arr = append(arr,root.Val)
+
     if root.Val == sum && root.Left == nil && root.Right == nil {
+        //slice是一个指向底层的数组的指针结构体
+        //因为是先序遍历，如果 root.Right != nil ,arr 切片底层的数组会被修改
+        //所以这里需要 copy arr 到 tmp，再添加进 ret，防止 arr 底层数据修改带来的错误
         tmp := make([]int,len(arr))
         copy(tmp,arr)
         *ret = append(*ret,tmp)
     }
+
     dfs(root.Left,sum - root.Val,arr,ret)
     dfs(root.Right,sum - root.Val,arr,ret)
+
     arr = arr[:len(arr)-1]
 }
+
 */
 
 func backTrack(root *TreeNode, current, sum int) bool {
